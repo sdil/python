fb53dbfbd0d9c7ecbf7b0f91eea81cf92d356f18
Fadhil Yaacob <mohamadfadhil.yaacob@onapp.com>
2020-03-10 15:43:28 +0800

    Fadhil's answer

--- exercises/etl/etl.py
+++ exercises/etl/etl.py
@@ -1,2 +1,8 @@
-def transform(legacy_data):
-    pass
+def transform(old):
+    new_data = {}
+    for i, (point, letters) in enumerate(old.items()):
+        for letter in letters:
+            new_data[letter.lower()] = point
+
+    return new_data
+
